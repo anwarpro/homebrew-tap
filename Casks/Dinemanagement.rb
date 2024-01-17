f678a35c7d14e0ab3f47610e4bd37d7bd1798bdf
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2024-01-17 06:09:20 +0000

    dine-management v1.0.4

--- Casks/Dinemanagement.rb
+++ Casks/Dinemanagement.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.9.0 at 2024-01-16T17:12:09.575953Z
+# Generated with JReleaser 1.9.0 at 2024-01-17T06:09:02.97453Z
 cask "dinemanagement" do
   desc "Effortless restaurant operations with intuitive dine management software."
   homepage "https://dine.helloanwar.com"
   url "https://github.com/anwarpro/dine-management-releases/releases/download/v1.0.4/dine-management-1.0.4.dmg",
       verified: "github.com"
   version "1.0.4"
-  sha256 "3177c79e1d210d3c363ad1d8e9c9b7f7eb4d05876b5c2fa4e15fd659d325ba0c"
+  sha256 "2b2cd65297e465dc7edd9c571effcff6869666a391c4f7bcaf7f25efa2e5f68c"
   name "Dine Management"
   auto_updates true
 
